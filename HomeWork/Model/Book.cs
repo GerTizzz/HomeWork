1d39dfb3efeda9531095f307967721014090dd43
Arseniy Muratov <mister.muratov@list.ru>
2022-01-26 23:28:11 +0500

    Добавил событие выбора в листбоксе

--- HomeWork/Model/Book.cs
+++ HomeWork/Model/Book.cs
@@ -1,20 +1,52 @@
 ﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace HomeWork.Model
 {
-    internal class Book
+    internal class Book : INotifyPropertyChanged
     {
         public string Name { get; set; }
         public int Id { get; set; }
 
-        public string Author { get; set; }
+        private string _Author;
+
+        public string Author
+        {
+            get => _Author;
+            set => SetOnPropertyChanged(ref _Author, value);
+        }
 
         public DateTime YearCreation;
 
         //public Image Cover { get; set; }
-        public string ISBN { get; set; }
+        private string _ISBN;
+
+        public string ISBN
+        {
+            get => _ISBN;
+            set => SetOnPropertyChanged(ref _ISBN, value);
+        }
 
         public string Description { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChange([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        public bool SetOnPropertyChanged<T>(ref T field, T value, [CallerMemberName] string PropertyName = null)
+        {
+            if (Equals(field, value))
+            {
+                return false;
+            }
+            field = value;
+            OnPropertyChange(PropertyName);
+            return true;
+        }
 
     }
 }
